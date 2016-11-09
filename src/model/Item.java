3afc149578b9318f85eb588ea968688681823b41
Vinit Patel <vinit9patel@gmail.com>
2016-11-09 10:48:14 -0700

    Item class written

--- src/model/Item.java
+++ src/model/Item.java
@@ -1,5 +1,25 @@
 package model;
 
 public abstract class Item {
-
+	private String itemName;
+	private int numOfItems;
+	
+	Item(String itemName){
+		this.itemName=itemName;
+	}
+	
+	public int getNumOfItems(String itemName){
+		return numOfItems;
+	}
+	
+	
+	public abstract void useItem();  
+	
+	
+	
+	
 }
+
+
+
+

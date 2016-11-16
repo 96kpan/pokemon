ff425e7c96634c3bf153c89701acc7a580393bda
96kpan <katie@dhcp-10-134-248-196.uawifi.arizona.edu>
2016-11-15 21:48:55 -0700

    done

---
+++ src/pokemons/Charmander.java
@@ -0,0 +1,14 @@
+package pokemons;
+
+import java.awt.Image;
+
+public class Charmander extends Pokemon {
+
+	public Charmander(int level, int totalHealth, String pokemonName, String pokemonType, boolean capturedOrNot,
+			int runProbs, Image pokemonPic) {
+		super(level, totalHealth, pokemonName, pokemonType, capturedOrNot, runProbs, pokemonPic);
+		// TODO Auto-generated constructor stub
+	}
+
+
+}
--- src/pokemons/Squirtle.java
+++ src/pokemons/Squirtle.java
@@ -2,12 +2,12 @@ package pokemons;
 
 import java.awt.Image;
 
-public class grassPokemon extends Pokemon {
+public class Squirtle extends Pokemon {
 
-	public grassPokemon(int level, int totalHealth, String pokemonName, String pokemonType, boolean capturedOrNot,
+	public Squirtle(int level, int totalHealth, String pokemonName, String pokemonType, boolean capturedOrNot,
 			int runProbs, Image pokemonPic) {
 		super(level, totalHealth, pokemonName, pokemonType, capturedOrNot, runProbs, pokemonPic);
+		
 	}
-
 
 }

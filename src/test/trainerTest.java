6ca68027d15efae44aa6814fbcbbd0c9f03df1f9
Niven Francis <nivenf@email.arizona.edu>
2016-11-15 21:11:45 -0700

    added trainer and bag classes with tests -nivenf

---
+++ src/test/trainerTest.java
@@ -0,0 +1,44 @@
+// Last updated: 11/15 2104
+// [WORKING ON BY NIVEN FRANCIS]
+
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import item.Item;
+import model.Trainer;
+
+public class trainerTest {
+	@Test
+	public void tester() {
+		Trainer t = new Trainer("Niven", null);
+		Item i1 = new Item("Bait");
+		Item i2 = new Item("Safari Ball");
+		Item i3 = new Item("Health pot");
+		
+		t.getBackpack().addItem(i1);
+		t.getBackpack().addItem(i2);
+		t.getBackpack().addItem(i3);
+		t.getBackpack().printItems();
+		t.getBackpack().addItem(i1);
+		t.getBackpack().removeItem(i1);
+		t.getBackpack().useItem(i2);
+		t.getBackpack().printItems();
+		t.getBackpack().numItems();
+		t.getBackpack().removeItem(i2);
+		t.getBackpack().removeItem(i3);
+		t.getBackpack().printItems();
+		
+		assertTrue(t.addSteps(1));
+		assertFalse(t.addSteps(499));
+		assertEquals(500, t.stepCount());
+		assertEquals("Niven", t.getName());
+		
+		t.throwBait();
+		t.throwBall();
+		t.throwRock();
+		t.run();
+	}
+}
